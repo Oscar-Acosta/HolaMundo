41042b3c02b149d4eb55d5374d64629564561471
Oscar-Acosta <oscar.acosta@versaria.com.mx>
2024-07-18 17:07:46 -0600

    Se agrega el proyecto

---
+++ src/main/java/com/arom/App.java
@@ -0,0 +1,15 @@
+package com.arom;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        JOptionPane.showMessageDialog(null, "Hola Mundo UWU");
+    }
+}
